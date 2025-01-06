c5c02a3b969adb7ff2009969506e3b2724d93ab9
Fatoniyah H <najwa.26252@gmail.com>
2025-01-06 09:37:08 +0700

    tap

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -13,7 +13,13 @@
             <div id="Overview" class="tabcontent active">
                 <div class="font-30 bold">Overview</div>
                 <hr>
-                <div class="text-grey">London is the capital city of England.</div>
+                <div class="text-grey text-format">Bali, also known as the land of gods has plenty to offer to travelers from across the
+                    globe. As it so contrasted oh estimating instrument. Size like body some one had. Are conduct viewing
+                    boy minutes warrant the expense? Tolerably behavior may admit daughters offending her ask own. Praise
+                    effect wishes change way and any wanted. Lively use looked latter regard had. Do he it part more last
+                    in. We understand that theory is important to build a solid foundation, we understand that theory alone
+                    isn’t going to get the job done so that’s why this is packed with practical hands-on examples that you
+                    can follow step by step.</div>
             </div>
             <div id="Itinerary" class="tabcontent">
                 <h3>Itinerary</h3>
